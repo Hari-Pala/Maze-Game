bad0b4bb005ece49f0df22d8b38d2e420ba1abe7
Jalp0495 <202101267@daiict.ac.in>
2022-07-23 18:11:26 +0530

    add solver

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+//Generator Part
+
 let blockid=1;
-let size=20;
+let size=10;
 let arr=[1];
 let arrcount=0;
+let ans=[];
+let check=1;
+var checkmat = new Array(size);
 
-document.getElementById("main").style.gridTemplateColumns="repeat("+size+",30px)";
-document.getElementById("main").style.gridTemplateRows="repeat("+size+",30px)";
+for (var i = 0; i < checkmat.length; i++) {
+  checkmat[i] = new Array(size).fill(0);
+}
+
+document.getElementById("main").style.gridTemplateColumns="repeat("+size+",10px)";
+document.getElementById("main").style.gridTemplateRows="repeat("+size+",10px)";
 for(let i=1;i<=size*size;i++){
-    document.getElementById("main").innerHTML+='<div class="block" id="'+i+'">'+i+'</div>';
+    document.getElementById("main").innerHTML+='<div class="block" id="'+i+'"></div>';
 }
 
 
-document.getElementById((blockid).toString()).style.backgroundColor='red'
+checkmat[(blockid-1)%size][(blockid-1)/size]=1;
 
 
 const next=()=>{
@@ -44,13 +53,12 @@ const next=()=>{
         }
     }
 }
-
 for (let index = 1; index <= size*size; index++) {
     document.getElementById(index.toString()).style.border="2px solid black";
 }
 
 while(1){
-    if((blockid-size<=0 || document.getElementById((blockid-size).toString()).style.backgroundColor=='red') && (blockid+size>=(size*size) || document.getElementById((blockid+size).toString()).style.backgroundColor=='red') && (blockid%size==1 || document.getElementById((blockid-1).toString()).style.backgroundColor=='red') && (blockid%size==0 || document.getElementById((blockid+1).toString()).style.backgroundColor=='red')){
+    if((blockid-size<=0 || checkmat[(blockid-size-1)%size][(blockid-size-1)/size]) && (blockid+size>=(size*size) || checkmat[(blockid+size-1)%size][(blockid+size-1)/size]) && (blockid%size==1 || checkmat[(blockid-1-1)%size][(blockid-1-1)/size]) && (blockid%size==0 || checkmat[(blockid-1+1)%size][(blockid-1+1)/size])){
         if(blockid-arr[arrcount-1]==size){
             document.getElementById(blockid.toString()).style.borderTop="";
             document.getElementById(arr[arrcount-1].toString()).style.borderBottom="";
@@ -76,16 +84,60 @@ while(1){
         continue;
     }
     next();
-    if(document.getElementById(blockid.toString()).style.backgroundColor=='red'){
+    if(checkmat[(blockid-1)%size][(blockid-1)/size]){
         blockid=arr[arrcount];
     }
     else{
-        document.getElementById(blockid.toString()).style.backgroundColor='red';
+        checkmat[(blockid-1)%size][(blockid-1)/size]=1;
         arr.push(blockid);
         arrcount++;
+        if((blockid==size*size) && check){
+            check=0;
+            for(let ind=0;ind<=arrcount;ind++){
+                ans.push(arr[ind]);
+            }
+        }
     }
 
     if(arr.lenth==0){
         break;
     }
+}
+
+
+
+//Solver Part
+
+const checkwin= ()=>{
+    let count=0;
+    for(let i=0;i<ans.length;i++){
+        if(document.getElementById(ans[i].toString()).style.backgroundColor!="red"){
+            return false;
+        }
+    }
+    for(let i=1;i<=size*size;i++){
+        if(document.getElementById(i.toString()).style.backgroundColor=="red"){
+            count++;
+        }
+    }
+    if(count==ans.length){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+for(let i=1;i<=size*size;i++){
+    document.getElementById(i.toString()).addEventListener('mouseover',()=>{
+        if(document.getElementById(i.toString()).style.backgroundColor=="red"){
+            document.getElementById(i.toString()).style.backgroundColor=="white";
+        }
+        else{
+            document.getElementById(i.toString()).style.backgroundColor="red";
+        }
+        if(checkwin()){
+            console.log("Done");
+        }
+    })
 }
